004f33c199408d4efe2b62f685539b1ab009f2c1
marrieta14 <marrieta@vicomtech.org>
2021-03-17 00:43:40 +0100

    27 Firebase Cloud Functions lesson finished.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,26 @@
 import * as functions from "firebase-functions";
+import express from 'express';
+import * as admin from 'firebase-admin';
+admin.initializeApp();
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+import Stripe from 'stripe';
+export const stripe = new Stripe(functions.config().stripe.secret, { 
+  apiVersion: '2020-03-02',
+});
+
+// Deploy the /server/api code to Firebase Cloud Functions as an Express App
+const app = express();
+export const api = functions.https.onRequest(app);
+
+// OR use callable functions instead of Express
+import {createStripeCheckoutSession} from './checkout';
+export const stripeCheckout = functions.https.onCall(async (data, context) => {
+  if (context.auth) {
+    // User is logged in
+  } else {
+    // User is NOT logged in
+  }
+
+  const checkoutSession = await createStripeCheckoutSession(data.line_items);
+  return checkoutSession;
+});
